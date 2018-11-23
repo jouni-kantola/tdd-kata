e90f88162746a021e5d5041d8a920bef1c4be7cb
Jouni Kantola <jouni@kantola.se>
2018-11-23 21:57:04 +0100

    100 students count to 100

--- fizz-buzz/fizz-buzz-tests.js
+++ fizz-buzz/fizz-buzz-tests.js
@@ -4,3 +4,8 @@ import fizzBuzz from "./";
 test("2 class mates count to 2", t => {
   t.deepEqual(fizzBuzz(2), [1, 2]);
 });
+
+test("100 class mates count to 100", t => {
+  const numberOfStudents = 100;
+  t.deepEqual(fizzBuzz(numberOfStudents).length, 100);
+});
